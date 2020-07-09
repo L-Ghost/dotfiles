7da096dfbb33c01f118d8f2610eecbc01010dea2
L-Ghost <luis.arantes.sp@gmail.com>
2020-07-09 19:36:40 -0300

    Add tmux basic config

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ DOTFILES_DIR=$PWD/$BASEDIR
 echo "CREATING SYMLINKS"
 echo $DOTFILES_DIR
 
+# vim file symlink
 ln -sf $DOTFILES_DIR/vimrc ~/.vimrc
+# tmux file symlink
+ln -sf $DOTFILES_DIR/tmux.conf ~/.tmux.conf
